aaa0e2f674f0b750d921a3dfe088503acb70adde
= <=>
2024-02-10 23:47:00 +0530

    chakra ui setup and skeletion

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,24 @@
+"use client";
+import { Link } from "@chakra-ui/next-js";
 import * as React from "react";
+import { isExternal } from "util/types";
+
+import Page from "@/components/index";
 
 export interface IHomePageProps {}
 
 export default function HomePage(props: IHomePageProps) {
   return (
     <div>
-      <h1>Chaitanya Mohite the DON</h1>
+      <Page />
+      <Link
+        href="www.google.com/"
+        color="blue.400"
+        _hover={{ color: "blue.500" }}
+        isExternal="True"
+      >
+        <h1>Chaitanya Mohite the DON</h1>
+      </Link>
     </div>
   );
 }
--- components/index.tsx
+++ components/index.tsx
@@ -1,7 +1,14 @@
 import React, { FC } from "react";
 
+import FormikExample from "./form";
+
 const Page: FC = () => {
-  return <div className="py-10">components file</div>;
+  return (
+    <div className="py-10">
+      components file
+      <FormikExample />
+    </div>
+  );
 };
 
 export default Page;
