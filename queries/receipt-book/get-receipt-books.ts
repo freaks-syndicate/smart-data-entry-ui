1dc43f80c0a6de5c3cd633a1aba771dbe8cec6a7
Atharva-CM <atharva.mahamuni@bluepineapple.io>
2024-03-03 09:45:27 +0530

    feat: getr receipt books query added

---
+++ queries/receipt-book/get-receipt-books.ts
@@ -0,0 +1,41 @@
+import { gql } from '@apollo/client';
+
+export const GET_RECEIPT_BOOKS = gql`
+  query ReceiptBooks($paginate: PaginationInput, $where: WhereOptionsReceiptBook) {
+    ReceiptBooks(paginate: $paginate, where: $where) {
+      results {
+        id
+        uuid
+        receiptBookNumber
+        receiptSeries
+        totalReceipts
+        financialYear
+        receipts {
+          id
+          uuid
+          receiptNumber
+          financialYear
+          date
+          mobileNumber
+          name
+          address
+          amount
+          modeOfPayment
+          aadharNumber
+          panNumber
+          idCode
+        }
+        usedReceipts
+      }
+      pageInfo {
+        hasNextPage
+        currentPage
+        hasPreviousPage
+        itemCount
+        pageCount
+        pageItemCount
+        perPage
+      }
+    }
+  }
+`;
