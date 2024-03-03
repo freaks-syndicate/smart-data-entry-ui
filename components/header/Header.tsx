17ebb305be5c8e3b62bb04bc0aa4b9b089d6413f
Atharva-CM <atharva.mahamuni@bluepineapple.io>
2024-03-03 13:14:29 +0530

    feat: add sign out button

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,22 +1,64 @@
 'use client';
+import { SettingTwoTone, UserOutlined } from '@ant-design/icons';
+import { Avatar, Button, Icon, Menu, MenuButton, MenuDivider, MenuGroup, MenuItem, MenuList, Skeleton, Text } from '@chakra-ui/react';
 import Image from 'next/image';
 import Link from 'next/link';
-import React, { useState } from 'react';
+import { useRouter } from 'next/navigation';
+import React, { useEffect, useState } from 'react';
+import { FaSignOutAlt } from 'react-icons/fa';
+import { useSessionContext } from 'supertokens-auth-react/recipe/session';
+import { signOut } from 'supertokens-auth-react/recipe/thirdpartyemailpassword';
+
+import { client } from '@/apollo/client.mjs';
+import { GET_USER_INFO } from '@/queries/user/get-user-info';
+import { IUserInfo } from '@/utils/types/be-model-types';
+import { IGetUserInfoResponse } from '@/utils/types/query-response.types';
 
 const Nav = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
+  const [userInfo, setUserInfo] = useState<IUserInfo | null>(null);
+  const session = useSessionContext();
+  const router = useRouter();
+
+  let userId: string | null = null;
+
+  if (!session.loading && session.doesSessionExist === false) {
+    router.push('/auth');
+  }
+
+  if (!session.loading) {
+    userId = session.userId;
+  }
+
+  useEffect(() => {
+    const getUserInfo = async () => {
+      if (!session.loading && userId) {
+        const { data } = await client.query<IGetUserInfoResponse>({ query: GET_USER_INFO, variables: { where: { userId: userId } } });
+        setUserInfo(data.user);
+      }
+    };
+
+    getUserInfo();
+  }, [session.loading, userId]);
+
+  async function onLogout() {
+    await signOut();
+    router.push('/auth');
+  }
 
   return (
     <>
       <div className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
-        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <div className="container flex items-center justify-between mx-auto p-4">
+          {/* Brand Name & Logo */}
           <Link href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
             <div className="relative w-8 h-8">
               <Image src="/sde-logo.png" alt="donation logo" width={32} height={32} />
             </div>
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Smart Data Entry</span>
           </Link>
-          <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+          {/* Toggle Menu Button */}
+          <div className="flex md:hidden space-x-3 md:space-x-0 rtl:space-x-reverse">
             <button
               data-collapse-toggle="navbar-sticky"
               type="button"
@@ -31,10 +73,8 @@ const Nav = () => {
               </svg>
             </button>
           </div>
-          <div
-            className={`items-center justify-between ${isMenuOpen ? 'flex' : 'hidden'} w-full md:flex md:w-10 md:order-1`}
-            id="navbar-sticky"
-          >
+          {/* Nav Links */}
+          <div className={`items-center justify-between ${isMenuOpen ? 'flex' : 'hidden'} md:flex`} id="navbar-sticky">
             <ul className="flex flex-col p-4 w-full md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
               <li>
                 <Link
@@ -62,6 +102,31 @@ const Nav = () => {
                 </Link>
               </li>
             </ul>
+          </div>
+          {/* Auth Section */}
+          <div className="flex gap-2">
+            <Skeleton isLoaded={!session.loading}>
+              <Menu>
+                <MenuButton as={Button}>
+                  <div className="flex items-center justify-center gap-2">
+                    <Text>Profile</Text>
+                    <Avatar size={'sm'} name={userInfo?.firstName} />
+                  </div>
+                </MenuButton>
+                <MenuList bgColor={'InfoBackground'}>
+                  <MenuGroup title={`${userInfo?.firstName} ${userInfo?.lastName}`}>
+                    <MenuItem icon={<Icon as={UserOutlined} />}>My Account</MenuItem>
+                    <MenuItem icon={<Icon as={SettingTwoTone} />}>Settings </MenuItem>
+                  </MenuGroup>
+                  <MenuDivider />
+                  <MenuGroup>
+                    <MenuItem icon={<Icon as={FaSignOutAlt} />} onClick={onLogout} color={'crimson'}>
+                      Sign Out
+                    </MenuItem>
+                  </MenuGroup>
+                </MenuList>
+              </Menu>
+            </Skeleton>
           </div>
         </div>
       </div>
