2e42999a3de27f6e019ab6fbc799e11db1ff4ce9
Atharva-CM <atharva.mahamuni@bluepineapple.io>
2024-03-14 23:02:31 +0530

    feat: added mutation types

--- utils/types/query-response.types.ts
+++ utils/types/query-response.types.ts
@@ -1,4 +1,4 @@
-import { IReceiptBookModel, IReceiptModel, IUserInfo } from './be-model-types';
+import { IReceiptBookModel, IReceiptModel, IUserInfo, ModeOfPayment } from './be-model-types';
 
 // Page Related
 export interface IPageInfo {
@@ -19,9 +19,69 @@ export interface IGetReceiptBooksResponse {
   };
 }
 
+export interface ICreateReceiptBookArgs {
+  item: {
+    financialYear?: string;
+    receiptBookNumber: number;
+    receiptSeries: number;
+    totalReceipts: number;
+  };
+}
+
+export interface IUpdateReceiptBookArgs {
+  id: string;
+  item: {
+    financialYear?: string;
+    receiptBookNumber?: number;
+    receiptSeries?: number;
+    totalReceipts?: number;
+  };
+}
+
+export interface IDeleteReceiptBookArgs {
+  id: string;
+}
+
 // Receipts
 export interface IGetReceiptsAllResponse {
   receipts: IReceiptModel[];
+}
+
+export interface ICreateReceiptArgs {
+  item: {
+    aadharNumber?: number;
+    address?: string;
+    amount: number;
+    date?: string;
+    financialYear?: number;
+    mobileNumber?: number;
+    modeOfPayment: ModeOfPayment;
+    name: string;
+    panNumber?: string;
+    receiptBookId: string;
+    receiptNumber: number;
+  };
+}
+
+export interface IUpdateReceiptArgs {
+  id: string;
+  item: {
+    aadharNumber?: number;
+    address?: string;
+    amount?: number;
+    date?: string;
+    financialYear?: number;
+    mobileNumber?: number;
+    modeOfPayment?: ModeOfPayment;
+    name?: string;
+    panNumber?: string;
+    receiptBookId?: string;
+    receiptNumber?: number;
+  };
+}
+
+export interface IDeleteReceiptArgs {
+  id: string;
 }
 
 // User
