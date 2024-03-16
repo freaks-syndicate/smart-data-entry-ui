bb2f74b5ce8fb965ef36fed8b5eb04d4cd02caa7
Atharva-CM <atharva.mahamuni@bluepineapple.io>
2024-03-16 23:22:06 +0530

    reafactor: changed create receipt route

--- app/books/[receiptBookNumber]/create-receipt/page.tsx
+++
@@ -1,5 +0,0 @@
-export interface ICreateReceiptPageProps {}
-
-export default function CreateReceiptPage(_props: ICreateReceiptPageProps) {
-  return <div>Create receipt page</div>;
-}
---
+++ app/books/[receiptBookNumber]/r/create/page.tsx
@@ -0,0 +1,11 @@
+import CreateReceiptTemplate from '@/components/templates/receipt-books/receipts/create';
+
+export interface ICreateReceiptPageProps {}
+
+export default function CreateReceiptPage(_props: ICreateReceiptPageProps) {
+  return (
+    <div>
+      <CreateReceiptTemplate />
+    </div>
+  );
+}
