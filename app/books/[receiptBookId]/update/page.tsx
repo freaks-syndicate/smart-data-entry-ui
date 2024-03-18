e0e35e98185fb7594117dbb4de7de2da00d768ed
Atharva-CM <atharva.mahamuni@bluepineapple.io>
2024-03-18 21:28:47 +0530

    feat: complete update receipt book page

--- app/books/[receiptBookId]/update/page.tsx
+++ app/books/[receiptBookId]/update/page.tsx
@@ -1,5 +1,27 @@
-export interface IUpdateReceiptBookPageProps {}
+import { client } from '@/apollo/client.mjs';
+import UpdateReceiptBookTemplate from '@/components/templates/receipt-books/update';
+import { GET_RECEIPT_BOOK } from '@/queries/receipt-book/get-receipt-book';
+import { IGetReceiptBookSingleArgs, IGetReceiptBookSingleResponse } from '@/utils/types/query-response.types';
 
-export default function UpdateReceiptBookPage(_props: IUpdateReceiptBookPageProps) {
-  return <div>update receipt book page</div>;
+export interface IUpdateReceiptBookPageProps {
+  params: {
+    receiptBookId: string;
+  };
+}
+
+export default async function UpdateReceiptBookPage(props: IUpdateReceiptBookPageProps) {
+  const {
+    params: { receiptBookId },
+  } = props;
+
+  const receiptBookResponse = await client.query<IGetReceiptBookSingleResponse, IGetReceiptBookSingleArgs>({
+    query: GET_RECEIPT_BOOK,
+    variables: { where: { id: receiptBookId } },
+  });
+
+  return (
+    <div>
+      <UpdateReceiptBookTemplate receiptBook={receiptBookResponse.data.receiptBook} />
+    </div>
+  );
 }
