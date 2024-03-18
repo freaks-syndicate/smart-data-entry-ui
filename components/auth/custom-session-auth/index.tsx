3a4e6183db50ea3106183384d95ced03d2bc810d
Atharva-CM <atharva.mahamuni@bluepineapple.io>
2024-03-18 09:45:54 +0530

    feat: add spinner at template level as auth session fallback

--- components/auth/custom-session-auth/index.tsx
+++ components/auth/custom-session-auth/index.tsx
@@ -1,5 +1,9 @@
+import { Spinner } from '@chakra-ui/react';
+import cx from 'classnames';
 import { Fragment, ReactNode } from 'react';
 import { useSessionContext } from 'supertokens-auth-react/recipe/session';
+
+import styles from './custom-session-auth.module.scss';
 
 export interface ICustomSessionAuthProps {
   children: ReactNode;
@@ -10,8 +14,13 @@ export default function CustomSessionAuth(props: ICustomSessionAuthProps) {
   const { children } = props;
   const session = useSessionContext();
 
+  // TODO: [UX] Get a fallback component as a prop, so that we can have page wise skeletons in place.
   if (session.loading) {
-    return <div>Loading...</div>;
+    return (
+      <div className={cx(styles['d-container'])}>
+        <Spinner thickness="4px" speed="0.65s" emptyColor="gray.200" color="blue.500" size="xl" />
+      </div>
+    );
   }
 
   if (session.doesSessionExist === false) {
