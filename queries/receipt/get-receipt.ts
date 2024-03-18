e59c4537b8dfb5603106dea8e2c652b24ab28a62
Atharva-CM <atharva.mahamuni@bluepineapple.io>
2024-03-18 22:05:26 +0530

    feat: added single receipt query

---
+++ queries/receipt/get-receipt.ts
@@ -0,0 +1,21 @@
+import { gql } from '@apollo/client';
+
+export const GET_RECEIPT = gql`
+  query Receipt($where: WhereOptionsReceipt) {
+    receipt: Receipt(where: $where) {
+      id
+      uuid
+      receiptNumber
+      financialYear
+      date
+      mobileNumber
+      name
+      address
+      amount
+      modeOfPayment
+      aadharNumber
+      panNumber
+      idCode
+    }
+  }
+`;
