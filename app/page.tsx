6454d646c2a1bd66fca210e1795303051234ce30
Atharva-CM <atharva.mahamuni@bluepineapple.io>
2024-03-21 09:35:25 +0530

    fix: ts error

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,18 @@
-import { ApolloQueryResult } from '@apollo/client';
-
 import { client } from '@/apollo/client.mjs';
 import ReceiptBooksTemplate from '@/components/templates/receipt-books';
-import { GET_RECEIPT_BOOKS } from '@/queries/receipt-book/get-receipt-books';
-import { IGetReceiptBooksResponse } from '@/utils/types/query-response.types';
+import { ReceiptBook, ReceiptBooksDocument, ReceiptBooksQueryResult, ReceiptBooksQueryVariables } from '@/utils/types/generated/graphql';
 
 export default async function HomePage() {
-  const receiptBooksResponse: ApolloQueryResult<IGetReceiptBooksResponse> = await client.query({
-    query: GET_RECEIPT_BOOKS,
+  const receiptBooksResponse = await client.query<ReceiptBooksQueryResult, ReceiptBooksQueryVariables>({
+    query: ReceiptBooksDocument,
   });
 
-  const receiptBooks = receiptBooksResponse?.data?.receiptBooks.results ?? [];
+  const receiptBooks = receiptBooksResponse.data.data?.receiptBooks?.results?.filter((book): book is ReceiptBook => book !== null) ?? [];
+
+  if (receiptBooks.length <= 0) {
+    console.error('[+] No Receipt Books Found');
+    return null;
+  }
 
   return (
     <div>
