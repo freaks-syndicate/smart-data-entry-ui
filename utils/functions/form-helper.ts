f0f0591f8df0238f5eb1f05c198f4b1449f91452
Atharva-CM <atharva.mahamuni@bluepineapple.io>
2024-04-28 13:09:21 +0530

    fix: infinite render on speech to text

--- utils/functions/form-helper.ts
+++ utils/functions/form-helper.ts
@@ -53,12 +53,19 @@ const handleDateInput = <T>(formData: T, fieldName: keyof T, value: string): T =
 
 const handleTextInput = <T>(formData: T, fieldName: keyof T, value: string): T => {
   let trimmedValue = value.trim();
+  const charLimits = {
+    panNumber: 10,
+    aadharNumber: 12,
+    mobileNumber: 10,
+  };
+  const limit = charLimits[fieldName as keyof typeof charLimits];
+
   if (fieldName === 'panNumber') {
     // For PAN number, convert to uppercase and remove all spaces
-    trimmedValue = toUpper(trimmedValue.replace(/\s+/g, ''));
+    trimmedValue = toUpper(trimmedValue.replace(/\s+/g, '').slice(0, limit));
   } else if (fieldName === 'mobileNumber' || fieldName === 'aadharNumber') {
     // For mobile and Aadhar numbers, remove all non-digit characters and spaces
-    trimmedValue = trimmedValue.replace(/\D+/g, '');
+    trimmedValue = trimmedValue.replace(/\D+/g, '').slice(0, limit);
   }
   return { ...formData, [fieldName]: trimmedValue } as T;
 };
